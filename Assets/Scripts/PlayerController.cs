26c1b16beeaeadd82a9c3d4ce1e3cb2432369f07
efernandezcostas <efernandezcostas@danielcastelao.org>
2025-02-12 09:02:17 +0100

    añadido salto, añadido multiplicador boost ramp, añadidos primeros estados

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,20 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-private Rigidbody rb; 
-private int count;
-private float movementX;
-private float movementY;
+   private Rigidbody rb; 
+   private int count;
+   private float movementX;
+   private float movementY;
+   private bool isGrounded;
 
-public float speed = 15f; 
-public TextMeshProUGUI countText;
-public GameObject winTextObject;
+   public float speed = 15f; 
+   public float jumpForce = 10f; // Fuerza del salto
+   public TextMeshProUGUI countText;
+   public GameObject winTextObject;
+   public float rampBoostMultiplier = 5f; // Multiplicador de velocidad al tocar la cuesta
 
+   private enum PlayerState { moving, jumping, invulnerable }
+   private PlayerState currentState;
 
    void Start()
    {
@@ -31,6 +36,15 @@ public GameObject winTextObject;
       movementX = movementVector.x; 
       movementY = movementVector.y; 
    }
+
+   void OnJump(InputValue jumpValue)
+   {
+      if (isGrounded)
+      {
+         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+         isGrounded = false;
+      }
+   }
    
    void SetCountText() 
    {
@@ -45,7 +59,7 @@ public GameObject winTextObject;
    private void FixedUpdate() 
    {
       Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-      rb.AddForce(movement * speed, ForceMode.Acceleration);
+      rb.AddForce(movement * speed);
    }
 
    void OnTriggerEnter(Collider other) 
@@ -56,6 +70,7 @@ public GameObject winTextObject;
          count = count + 1;
          SetCountText();
       }
+   
    }
 
    private void OnCollisionEnter(Collision collision)
@@ -67,6 +82,16 @@ public GameObject winTextObject;
          // Update the winText to display "You Lose!"
          winTextObject.gameObject.SetActive(true);
          winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+      }
+      else if (collision.gameObject.CompareTag("Ramp"))
+      {
+            // Aplica un impulso adicional al jugador al tocar la cuesta
+            Vector3 boost = new Vector3(movementX, 0.0f, movementY) * rampBoostMultiplier;
+            rb.AddForce(boost, ForceMode.Impulse);
+      }
+      else if (collision.gameObject.CompareTag("Ground"))
+      {
+         isGrounded = true;
       }
    }
 }
